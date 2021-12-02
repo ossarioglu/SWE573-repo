babdd3a9f83b1de6d3e5923477d895a984d35ead
Osman Selçuk Sarıoğlu <ossarioglu@gmail.com>
2021-12-03 00:15:28 +0300

    Template folder created. URL routings are done. Views are created

--- socializeUs/base/views.py
+++ socializeUs/base/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# My views
+
+def home(request):
+    return render(request, 'home.html')
+
+def offerings(request):
+    return render(request, 'offerings.html')
+#    return HttpResponse('Offering Page')
+
