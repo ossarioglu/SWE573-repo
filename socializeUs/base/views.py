1d923543bdb7900ac1d2d9bbaee349eb923a5288
Osman Selçuk Sarıoğlu <ossarioglu@gmail.com>
2021-12-09 00:05:21 +0300

    Search feature by tag is added

--- socializeUs/base/views.py
+++ socializeUs/base/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from .models import Offering
+from .models import Offering, Tag
 from .forms import OfferForm
 # My views
 
 
 def home(request):
-    offers = Offering.objects.all()
-    context = {'offers':offers}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    offers = Offering.objects.filter(tag__tagName__contains=q)
+    tags = Tag.objects.all()
+    
+    context = {'offers':offers, 'tags':tags}
     return render(request, 'base/home.html', context)
 
 def offerings(request, ofnum):
