556144cc6a37e7f06bb6d8e758f3197144e33684
Osman Selçuk Sarıoğlu <ossarioglu@gmail.com>
2022-01-09 09:00:15 +0300

    deleted old files

--- socializeUs/base/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm, fields
-from .models import Offering
-
-class OfferForm(ModelForm):
-    class Meta:
-        model = Offering
-        fields = '__all__'
-        exclude = ['providerID']
